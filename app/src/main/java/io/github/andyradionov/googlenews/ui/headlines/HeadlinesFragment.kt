d728525e9987ab1bb8cfe8d3a0088eaeb3680e1f
Andrey Radionov <andyomsk@gmail.com>
2018-11-06 15:30:45 +0600

    Headlines tabs update

--- app/src/main/java/io/github/andyradionov/googlenews/ui/headlines/HeadlinesFragment.kt
+++ app/src/main/java/io/github/andyradionov/googlenews/ui/headlines/HeadlinesFragment.kt
@@ -1,29 +1,19 @@
 package io.github.andyradionov.googlenews.ui.headlines
 
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.view.ViewPager
-import android.support.v7.widget.GridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.arellomobile.mvp.presenter.InjectPresenter
-import com.arellomobile.mvp.presenter.ProvidePresenter
 import io.github.andyradionov.googlenews.R
-import io.github.andyradionov.googlenews.data.entities.Article
 import io.github.andyradionov.googlenews.ui.common.BaseFragment
-import io.github.andyradionov.googlenews.ui.topnews.NewsAdapter
-import io.github.andyradionov.googlenews.ui.topnews.TopNewsPresenter
-import javax.inject.Inject
-
 
 class HeadlinesFragment : BaseFragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var tabLayout: TabLayout
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -31,12 +21,23 @@ class HeadlinesFragment : BaseFragment() {
         return inflater.inflate(R.layout.fragment_headlines, container, false)
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        val activity = context as Activity
+        tabLayout = activity.findViewById(R.id.tabs)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val viewPager = view.findViewById<ViewPager>(R.id.viewpager)
         viewPager.adapter = HeadlinesPageAdapter(context!!, childFragmentManager)
-        viewPager.offscreenPageLimit = 0
-        val tabLayout = view.findViewById<TabLayout>(R.id.tabs)
+
         tabLayout.setupWithViewPager(viewPager)
+        tabLayout.visibility = View.VISIBLE
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        tabLayout.visibility = View.GONE
     }
 }
