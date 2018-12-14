8e7696605547e63a883181ade8fd8df14d290e47
Andrey Radionov <andyomsk@gmail.com>
2018-12-14 23:42:03 +0600

    ListAdapter

--- app/src/main/java/io/github/andyradionov/googlenews/ui/common/adapter/NewsAdapter.kt
+++ app/src/main/java/io/github/andyradionov/googlenews/ui/common/adapter/NewsAdapter.kt
@@ -1,6 +1,6 @@
 package io.github.andyradionov.googlenews.ui.common.adapter
 
-import android.support.v7.recyclerview.extensions.AsyncListDiffer
+import android.support.v7.recyclerview.extensions.ListAdapter
 import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -14,17 +14,12 @@ import kotlinx.android.synthetic.main.item_article.view.*
 /**
  * @author Andrey Radionov
  */
-class NewsAdapter(private val clickListener: OnArticleClickListener) : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
-
-    private val differ = AsyncListDiffer(this, ArticlesDiffCallback())
+class NewsAdapter(private val clickListener: OnArticleClickListener) :
+        ListAdapter<Article, NewsAdapter.ArticleViewHolder>(ArticlesDiffCallback()) {
 
     interface OnArticleClickListener {
         fun onClick(articleUrl: String)
         fun onOpenDialogClick(articleUrl: String, isFavourite: Boolean)
-    }
-
-    fun updateData(articles: List<Article>) {
-        differ.submitList(articles)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
@@ -37,17 +32,13 @@ class NewsAdapter(private val clickListener: OnArticleClickListener) : RecyclerV
         holder.bind(position)
     }
 
-    override fun getItemCount(): Int {
-        return differ.currentList.size
-    }
-
     inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
             View.OnClickListener {
 
         fun bind(position: Int) {
             itemView.setOnClickListener(this)
             itemView.iv_open_dialog.setOnClickListener(this)
-            val article = differ.currentList[position]
+            val article = getItem(position)
 
             itemView.tv_article_title.text = article.title
 
@@ -60,7 +51,7 @@ class NewsAdapter(private val clickListener: OnArticleClickListener) : RecyclerV
         }
 
         override fun onClick(v: View) {
-            val article = differ.currentList[adapterPosition]
+            val article = getItem(adapterPosition)
             when(v) {
                 itemView.iv_open_dialog ->
                     clickListener.onOpenDialogClick(article.url, article.isFavourite)
