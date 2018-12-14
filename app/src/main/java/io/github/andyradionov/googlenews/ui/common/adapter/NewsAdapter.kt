a95026ada718bbeaa1e542b5033dffb0924ad468
Andrey Radionov <andyomsk@gmail.com>
2018-12-15 00:26:47 +0600

    AdapterDelegate implemented

--- app/src/main/java/io/github/andyradionov/googlenews/ui/common/adapter/NewsAdapter.kt
+++ app/src/main/java/io/github/andyradionov/googlenews/ui/common/adapter/NewsAdapter.kt
@@ -1,62 +1,15 @@
 package io.github.andyradionov.googlenews.ui.common.adapter
 
-import android.support.v7.recyclerview.extensions.ListAdapter
-import android.support.v7.widget.CardView
-import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.squareup.picasso.Picasso
-import io.github.andyradionov.googlenews.R
+import com.hannesdorfmann.adapterdelegates3.AsyncListDifferDelegationAdapter
 import io.github.andyradionov.googlenews.data.entities.Article
-import kotlinx.android.synthetic.main.item_article.view.*
 
 /**
  * @author Andrey Radionov
  */
-class NewsAdapter(private val clickListener: OnArticleClickListener) :
-        ListAdapter<Article, NewsAdapter.ArticleViewHolder>(ArticlesDiffCallback()) {
+class NewsAdapter(clickListener: NewsAdapterDelegate.OnArticleClickListener) :
+        AsyncListDifferDelegationAdapter<Article>(ArticlesDiffCallback()) {
 
-    interface OnArticleClickListener {
-        fun onClick(articleUrl: String)
-        fun onOpenDialogClick(articleUrl: String, isFavourite: Boolean)
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
-        val cardView = LayoutInflater.from(parent.context)
-                .inflate(R.layout.item_article, parent, false) as CardView
-        return ArticleViewHolder(cardView)
-    }
-
-    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
-        holder.bind(position)
-    }
-
-    inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
-            View.OnClickListener {
-
-        fun bind(position: Int) {
-            itemView.setOnClickListener(this)
-            itemView.iv_open_dialog.setOnClickListener(this)
-            val article = getItem(position)
-
-            itemView.tv_article_title.text = article.title
-
-            Picasso.get()
-                    .load(article.urlToImage)
-                    .placeholder(R.drawable.error_placeholder)
-                    .centerCrop()
-                    .fit()
-                    .into(itemView.iv_article_image)
-        }
-
-        override fun onClick(v: View) {
-            val article = getItem(adapterPosition)
-            when(v) {
-                itemView.iv_open_dialog ->
-                    clickListener.onOpenDialogClick(article.url, article.isFavourite)
-                else -> clickListener.onClick(article.url)
-            }
-        }
+    init {
+        delegatesManager.addDelegate(NewsAdapterDelegate(clickListener))
     }
 }
---
+++ app/src/main/java/io/github/andyradionov/googlenews/ui/common/adapter/NewsAdapterDelegate.kt
@@ -0,0 +1,75 @@
+package io.github.andyradionov.googlenews.ui.common.adapter
+
+import android.support.v7.recyclerview.extensions.ListAdapter
+import android.support.v7.widget.CardView
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hannesdorfmann.adapterdelegates3.AdapterDelegate
+import com.squareup.picasso.Picasso
+import io.github.andyradionov.googlenews.R
+import io.github.andyradionov.googlenews.data.entities.Article
+import kotlinx.android.synthetic.main.item_article.view.*
+
+/**
+ * @author Andrey Radionov
+ */
+class NewsAdapterDelegate(private val clickListener: OnArticleClickListener) :
+        AdapterDelegate<List<Article>>() {
+
+    interface OnArticleClickListener {
+        fun onClick(articleUrl: String)
+        fun onOpenDialogClick(articleUrl: String, isFavourite: Boolean)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder {
+        val cardView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_article, parent, false) as CardView
+        return ArticleViewHolder(cardView)
+    }
+
+    override fun onBindViewHolder(items: List<Article>, position: Int, holder: RecyclerView.ViewHolder, payloads: MutableList<Any>) {
+        val articlesViewHolder = holder as ArticleViewHolder
+
+        articlesViewHolder.bind(items[position])
+    }
+
+    override fun isForViewType(items: List<Article>, position: Int): Boolean {
+        return items[position] is Article
+    }
+
+    inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+            View.OnClickListener {
+
+        private var article: Article? = null
+
+        init {
+            itemView.setOnClickListener(this)
+            itemView.iv_open_dialog.setOnClickListener(this)
+        }
+
+        fun bind(article: Article) {
+            this.article = article
+            itemView.tv_article_title.text = article.title
+
+            Picasso.get()
+                    .load(article.urlToImage)
+                    .placeholder(R.drawable.error_placeholder)
+                    .centerCrop()
+                    .fit()
+                    .into(itemView.iv_article_image)
+        }
+
+        override fun onClick(v: View) {
+            article?.let {
+                when(v) {
+                    itemView.iv_open_dialog ->
+                        clickListener.onOpenDialogClick(it.url, it.isFavourite)
+                    else -> clickListener.onClick(it.url)
+                }
+            }
+
+        }
+    }
+}
