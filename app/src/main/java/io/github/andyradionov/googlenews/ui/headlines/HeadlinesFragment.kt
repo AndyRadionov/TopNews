71131d90a39f8376fd910b2ad77580d1efdb0069
Andrey Radionov <andyomsk@gmail.com>
2018-12-23 23:15:59 +0600

    Rotation categories fix

--- app/src/main/java/io/github/andyradionov/googlenews/ui/headlines/HeadlinesFragment.kt
+++ app/src/main/java/io/github/andyradionov/googlenews/ui/headlines/HeadlinesFragment.kt
@@ -10,10 +10,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.arellomobile.mvp.MvpAppCompatFragment
 import io.github.andyradionov.googlenews.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HeadlinesFragment : MvpAppCompatFragment() {
-
-    private lateinit var tabLayout: TabLayout
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -21,23 +20,17 @@ class HeadlinesFragment : MvpAppCompatFragment() {
         return inflater.inflate(R.layout.fragment_headlines, container, false)
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        val activity = context as Activity
-        tabLayout = activity.findViewById(R.id.tabs)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val viewPager = view.findViewById<ViewPager>(R.id.viewpager)
         viewPager.adapter = HeadlinesPageAdapter(context!!, childFragmentManager)
 
-        tabLayout.setupWithViewPager(viewPager)
-        tabLayout.visibility = View.VISIBLE
+        activity?.tabs?.setupWithViewPager(viewPager)
+        activity?.tabs?.visibility = View.VISIBLE
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        tabLayout.visibility = View.GONE
+        activity?.tabs?.visibility = View.GONE
     }
 }
